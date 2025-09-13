f5482713056b9953f09e84eb35d8aa40881e8941
Jean Lucca <jlfreitas25@gmail.com>
2025-09-13 09:29:09 -0300

    adicionando o exercicio 14

--- exercicios.py
+++ exercicios.py
@@ -33,11 +33,11 @@ import math
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
-raio_do_circulo = float(input("Digite o raio do círculo: "))
-area_do_circulo =  math.pi  * raio_do_circulo ** 2
+#raio_do_circulo = float(input("Digite o raio do círculo: "))
+#area_do_circulo =  math.pi  * raio_do_circulo ** 2
 #area_do_circulo_formatada = "{:.2f}".format(area_do_circulo)
 #print(f"A area do circulo de raio {raio_do_circulo} eh: {area_do_circulo_formatada}")
-print(f"{area_do_circulo:.2f}")
+#print(f"{area_do_circulo:.2f}")
 
 # #### Strings (`str`)
 
@@ -45,6 +45,16 @@ print(f"{area_do_circulo:.2f}")
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+
+data_do_usuario = input("Insira uma data no formato dd/mm/aaaa: ")
+lista_de_dia_mes_ano = data_do_usuario.split("/")
+print(f"dia: {lista_de_dia_mes_ano[0]}")
+print(f"mes: {lista_de_dia_mes_ano[1]}")    
+print(f"ano: {lista_de_dia_mes_ano[2]}")
+print(f"Nasceu no dia: {lista_de_dia_mes_ano[0]}, no mes: {lista_de_dia_mes_ano[1]}, no ano: {lista_de_dia_mes_ano[2]}") #ou print(f"Nasceu no dia: {lista_de_dia_mes_ano[0]}, mes: {lista_de_dia_mes_ano[1]}, ano: {lista_de_dia_mes_ano[2]}")
+
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
