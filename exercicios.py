0abd95db5214a76322f0c8c45cecb57db854de51
Jean Lucca <jlfreitas25@gmail.com>
2025-09-13 09:53:33 -0300

    adicionando os exercicios de inteiros e pontos flutuantes

--- exercicios.py
+++ exercicios.py
@@ -15,29 +15,66 @@ import math
   # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
-# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-#numero_1 = int(input("Inserir um número inteiro: "))
-#numero_2 = int(input("Inserir outro número inteiro: "))
+int_1 = int(input("Inserir um número inteiro: "))
+int_2 = int(input("Inserir outro número inteiro: "))
+soma = int_1 + int_2
+print(f"O resultado da soma entre {int_1} e {int_2} é: {soma}")
 
-#resultado = numero_1 // numero_2
-#print(f"O resultado da divisão inteira entre {numero_1} e {numero_2} é: {resultado}")
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+num= int(input("Digite um número: "))
+resto = num % 5
+print(f"O resto da divisao de {num} por 5 é: {resto}")
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+num_01 = int(input("Inserir um número inteiro: "))
+num_02 = int(input("Inserir outro número inteiro: "))
+multiplicado = num_01 * num_02
+print(f"O resultado da multiplicação entre {num_01} e {num_02} é: {multiplicado}")
+
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+numero_1 = int(input("Inserir um número inteiro: "))
+numero_2 = int(input("Inserir outro número inteiro: "))
+
+resultado = numero_1 // numero_2
+print(f"O resultado da divisão inteira entre {numero_1} e {numero_2} é: {resultado}")
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+num = int(input("Inserir um número inteiro: "))
+resultado_quadrado = num ** 2
+print(f"O quadrado de {num} é: {resultado_quadrado}")
 
 # #### Números de Ponto Flutuante (`float`)
 
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+num_1 = float(input("Inserir um número flutuante: "))
+num_2 = float(input("Inserir outro número flutuante: "))
+soma = num_1 + num_2
+print(f"O resultado da soma entre {num_1} e {num_2} é: {soma}")
+
+
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+num_1 = float(input("Inserir um número flutuante: "))
+num_2 = float(input("Inserir outro número flutuante: "))
+media = (num_1 + num_2) / 2
+print(f"A media de {num_1} e {num_2} é: {media}")
+
+
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+base = float(input("Digite a base: "))
+expoente = float(input("Digite o expoente: "))
+potencia = base ** expoente
+print(f"A potência de {base} elevado a {expoente} é: {potencia}")
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+celsius = float(input("Digite a temperatura em Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius}°C é igual a {fahrenheit}°F")
+
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
-#raio_do_circulo = float(input("Digite o raio do círculo: "))
-#area_do_circulo =  math.pi  * raio_do_circulo ** 2
+raio_do_circulo = float(input("Digite o raio do círculo: "))
+area_do_circulo =  math.pi  * raio_do_circulo ** 2
 #area_do_circulo_formatada = "{:.2f}".format(area_do_circulo)
 #print(f"A area do circulo de raio {raio_do_circulo} eh: {area_do_circulo_formatada}")
-#print(f"{area_do_circulo:.2f}")
+print(f"{area_do_circulo:.2f}")
 
 # #### Strings (`str`)
 
@@ -47,12 +84,12 @@ import math
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 
 
-data_do_usuario = input("Insira uma data no formato dd/mm/aaaa: ")
-lista_de_dia_mes_ano = data_do_usuario.split("/")
-print(f"dia: {lista_de_dia_mes_ano[0]}")
-print(f"mes: {lista_de_dia_mes_ano[1]}")    
-print(f"ano: {lista_de_dia_mes_ano[2]}")
-print(f"Nasceu no dia: {lista_de_dia_mes_ano[0]}, no mes: {lista_de_dia_mes_ano[1]}, no ano: {lista_de_dia_mes_ano[2]}") #ou print(f"Nasceu no dia: {lista_de_dia_mes_ano[0]}, mes: {lista_de_dia_mes_ano[1]}, ano: {lista_de_dia_mes_ano[2]}")
+#data_do_usuario = input("Insira uma data no formato dd/mm/aaaa: ")
+#lista_de_dia_mes_ano = data_do_usuario.split("/")
+#print(f"dia: {lista_de_dia_mes_ano[0]}")
+#print(f"mes: {lista_de_dia_mes_ano[1]}")    
+#print(f"ano: {lista_de_dia_mes_ano[2]}")
+#print(f"Nasceu no dia: {lista_de_dia_mes_ano[0]}, no mes: {lista_de_dia_mes_ano[1]}, no ano: {lista_de_dia_mes_ano[2]}") #ou print(f"Nasceu no dia: {lista_de_dia_mes_ano[0]}, mes: {lista_de_dia_mes_ano[1]}, ano: {lista_de_dia_mes_ano[2]}")
 
 
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
